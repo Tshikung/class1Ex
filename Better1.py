450b88f6fc200314774792c335d6a6410937fb9f
Tshikung <munyikotshikung@gmail.com>
2024-09-17 08:26:58 +0300

    Add files via upload

---
+++ Better1.py
@@ -0,0 +1,9 @@
+# # Phone Class Example 
+# class Phone:
+#     # Constructor to initialaze the Object
+#     def __init__(self) -> None:
+#         pass
+    
+va = input('your name please!')
+
+print(va)
